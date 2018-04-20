edcc94359e5dcc665f15125e5102c49ec6b34d8b
Trc1993 <1129440815@qq.com>
2018-04-20 18:50:08 +0800

    no message

---
+++ user/src/main/java/com/trc/user/LoginActivity.java
@@ -0,0 +1,23 @@
+package com.trc.user;
+
+import android.os.Bundle;
+
+import com.trc.base.BaseActivity;
+
+public class LoginActivity extends BaseActivity {
+
+    @Override
+    public int intiLayout() {
+        return R.layout.activity_login;
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+}
